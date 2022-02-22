d0c9137f66d678455691e7a3a6b3ff601757eafd
Mehdi Ben ahmed <79095612+mehdi147899@users.noreply.github.com>
2022-02-22 01:26:59 +0100

    Add files via upload

--- fournisseur/mainwindow.cpp
+++ fournisseur/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QMessageBox>
+#include <QPixmap>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/LENOVO/Documents/fournisseur/logo.png");
+   ui->logo0->setPixmap(pix);
 }
 
 MainWindow::~MainWindow()
