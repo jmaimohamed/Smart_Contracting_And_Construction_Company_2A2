27065fff72e57480b7bb7c5676b05283f9e5a535
Yassine Derbel <79099282+IWCKD1I@users.noreply.github.com>
2022-04-04 14:49:59 +0100

    Chat et Qr code

---
+++ messages.cpp
@@ -0,0 +1,6 @@
+#include "messages.h"
+
+Messages::Messages()
+{
+
+}
---
+++ messages.h
@@ -0,0 +1,11 @@
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+
+class Messages
+{
+public:
+    Messages();
+};
+
+#endif // MESSAGES_H
